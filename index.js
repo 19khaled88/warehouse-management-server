f44561a96f718057acc15e8777ddb852e1f6c689
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-04-30 22:31:25 +0600

    get items

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const cors = require('cors')
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 5000
+const ObjectId = require('mongodb').ObjectId
 //for responding to client request
+var objectId = new ObjectId()
 app.use(cors())
 
 //for body parsing
@@ -16,6 +19,9 @@ app.get('/', (req, res) => {
 
 //user:
 //pass:
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ka5da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+// const uri = `mongodb+srv://khaled:VNHAybzMnVDF6NMq@cluster0.ka5da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ka5da.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
@@ -27,28 +33,109 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect()
+
     const productCollection = client.db('warehouse').collection('product')
+    const stockCollection = client.db('warehouse').collection('stock')
 
-    app.get('/product', (req, res) => {})
-
-    app.post('/product', (req, res) => {
-      const data = req.body
-      console.log(data)
-      res.send('data posted to mongodb')
+    // Home items
+    app.get('/getHomeItems', async (req, res) => {
+      const query = {}
+      const limit = 6
+      const cursor = productCollection.find(query).limit(limit)
+      const items = await cursor.toArray()
+      res.send(items)
     })
 
-    app.post('/stock', async (req, res) => {
-      const data = req.body
-      console.log(data)
-      const result = await productCollection.insertOne(data)
+    // single item
+    app.get('/getInventory/:id', async (req, res) => {
+      const id = req.params.id
+      var o_id = new ObjectId(id)
+      // const query = { _id: ObjectId(id) }
+    
+      const cursor = await productCollection.find(ObjectId(id))
+
+      res.send(cursor)
+    })
+
+    // manage inventory items
+    app.get('/getItems', async (req, res) => {
+      const query = {}
+      const cursor = productCollection.find(query)
+      const items = await cursor.toArray()
+      res.send(items)
+    })
+
+    // index posts
+    app.get('/stock', async (req, res) => {
+      const query = {}
+      const cursor = postCollection.find(query)
+      const posts = await cursor.toArray()
+      res.send(posts)
+    })
+    // index posts
+    app.get('/postsCount', async (req, res) => {
+      const query = {}
+      const cursor = postCollection.find(query)
+      const posts = await cursor.count()
+      res.send({ posts })
+    })
+
+    // create new item
+    app.post('/addItem', async (req, res) => {
+      const newUser = req.body
+
+      const result = await productCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    // delete items
+    app.delete('/item/:itemId', async (req, res) => {
+      // const id = req.body;
+      const id = req.params.itemId
+
+      const query = { _id: ObjectId(id) }
+
+      const result = await productCollection.deleteOne(query)
+      if (result.deletedCount === 1) {
+        console.log('Successfully deleted one document.')
+      } else {
+        console.log('No documents matched the query. Deleted 0 documents.')
+      }
+      // res.send(result);
+    })
+
+    // update user
+    app.put('/user/:usrId', async (req, res) => {
+      // const id = req.body;
+      const id = req.params.usrId
+
+      const query = { _id: ObjectId(id) }
+
+      const options = { upsert: true }
+
+      const updateDoc = {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+        },
+      }
+      const result = await userCollection.updateOne(query, updateDoc, options)
+      if (result.deletedCount === 1) {
+        console.log('Successfully deleted one document.')
+      } else {
+        console.log('No documents matched the query. Deleted 0 documents.')
+      }
       res.send(result)
     })
   } finally {
-    // await client.close()
+    // await client.close();
   }
 }
 run().catch(console.dir)
 
+app.get('/', (req, res) => {
+  res.send('Running my mongo db')
+})
 app.listen(port, () => {
   console.log('listening server to port', port)
 })
